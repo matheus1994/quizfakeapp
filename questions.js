c8b6bfde1c8f913dbd5418d354a1339d7778eaed
matheus1994 <31566239+matheus1994@users.noreply.github.com>
2023-11-29 13:35:47 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,86 @@
+export default [
+  
+  {
+   question: "Disseram que o Brasil não possui praias?",
+   answers:[
+   {option: "Sim", correct: false},
+   {option: "Não", correct: true},
+   ],
+  }, 
+
+
+  {
+   question: "Disseram que o Brasil não possui floresta amazônica?",
+   answers:[
+   {option: "Sim", correct: false},
+   {option: "Não", correct: true},
+   ],
+  }, 
+
+  {
+   question: "Brasília é a capital do Brasil?",
+   answers:[
+   {option: "Sim", correct: true},
+   {option: "Não", correct: false},
+   ],
+  }, 
+  
+
+  {
+   question: "O Brasil faz parte do BRICS?",
+   answers:[
+   {option: "Sim", correct: true},
+   {option: "Não", correct: false},
+   ],
+  },
+
+  {
+   question: "O Brasil não possui presidente da república?",
+   answers:[
+   {option: "Sim", correct: false},
+   {option: "Não", correct: true},
+   ],
+  }, 
+
+  {
+   question: "O Brasil é um país europeu?",
+   answers:[
+   {option: "Sim", correct: false},
+   {option: "Não", correct: true},
+   ],
+  }, 
+
+  {
+   question: "O IFBA não possui professores?",
+   answers:[
+   {option: "Sim", correct: false},
+   {option: "Não", correct: true},
+   ],
+  }, 
+
+  {
+   question: "O IFBA não possui professores?",
+   answers:[
+   {option: "Sim", correct: false},
+   {option: "Não", correct: true},
+   ],
+  }, 
+
+  {
+   question: "O IFBA é uma instituição de ensino pública?",
+   answers:[
+   {option: "Sim", correct: true},
+   {option: "Não", correct: false},
+   ],
+  }, 
+
+  {
+   question: "O IFBA possui ensino superior?",
+   answers:[
+   {option: "Sim", correct: true},
+   {option: "Não", correct: false},
+   ],
+  }, 
+
+
+]
